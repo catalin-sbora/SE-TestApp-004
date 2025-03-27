ea87472b9ce34f02157bcd9d56e19d9acbdfa361
catalin-sbora <catalin.sbora@gmail.com>
2025-03-27 09:01:35 +0200

    Update Program.cs

--- TestWebApp/Program.cs
+++ TestWebApp/Program.cs
@@ -5,7 +5,7 @@ namespace TestWebApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            hjashdjashdjasd asda sdasdasd;
             // Add services to the container.
             builder.Services.AddRazorPages();
 
